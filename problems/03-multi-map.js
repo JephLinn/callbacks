29edb35ad6ab8ffbb24a3cc26b7052a3c5395d1e
Jeffrey Manning <jeffmgpfit@yahoo.com>
2023-11-24 08:46:49 -0500

    03 done

--- problems/03-multi-map.js
+++ problems/03-multi-map.js
@@ -21,8 +21,14 @@ let result3 = multiMap("hi", 5, function(s) {
 console.log(result3); // hi!!!!!
 *******************************************************************************/
 
-let multiMap = function() {
+let multiMap = function(val, n, cb) {
+  let newVal = val;
 
+  for (let i = 0; i < n; i++) {
+    let res = cb(newVal);
+    newVal = res;
+  }
+  return newVal;
 };
 
 
